50db794df2e94955b790de30fa880aeaf9fb4a85
fmy-xfk <92439767+fmy-xfk@users.noreply.github.com>
2026-08-29 13:28:00 +0800

    client mixin fix

--- src/client/java/cn/myfrank/stationbuilder/mixin/client/mtr/RenderRailsMixin.java
+++ src/client/java/cn/myfrank/stationbuilder/mixin/client/mtr/RenderRailsMixin.java
@@ -1,14 +1,9 @@
 package cn.myfrank.stationbuilder.mixin.client.mtr;
 
-import net.minecraft.block.Block;
+import cn.myfrank.stationbuilder.RailBuilderItem;
+import cn.myfrank.stationbuilder.StationBuilderBlock;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.item.Item;
-import org.mtr.block.BlockNode;
-import org.mtr.block.BlockSignalLightBase;
-import org.mtr.block.BlockSignalSemaphoreBase;
-import org.mtr.block.PlatformHelper;
-import org.mtr.item.ItemBrush;
-import org.mtr.item.ItemNodeModifierBase;
 import org.mtr.render.RenderRails;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -40,7 +35,7 @@ public class RenderRailsMixin {
     private static boolean isHoldingMyCustomRailThing(ClientPlayerEntity clientPlayerEntity) {
         return clientPlayerEntity.isHolding((itemStack) -> {
             Item item = itemStack.getItem();
-            return item instanceof ItemNodeModifierBase || item instanceof ItemBrush || Block.getBlockFromItem(item) instanceof BlockSignalLightBase || Block.getBlockFromItem(item) instanceof BlockNode || Block.getBlockFromItem(item) instanceof BlockSignalSemaphoreBase || Block.getBlockFromItem(item) instanceof PlatformHelper;
+            return item instanceof RailBuilderItem || net.minecraft.block.Block.getBlockFromItem(item) instanceof StationBuilderBlock;
         });
     }
 }
